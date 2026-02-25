0a9a367ef6397407939d48e0ab0f35ea8cb7f708
artur-silva-empresa <artur.silva.empresa@gmail.com>
2026-02-25 23:31:24 +0000

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: './',
+      base: '/Lasa-app/',
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -18,6 +18,16 @@ export default defineConfig(({ mode }) => {
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
+        }
+      },
+      optimizeDeps: {
+        exclude: ['sql.js']
+      },
+      build: {
+        rollupOptions: {
+          output: {
+            manualChunks: undefined
+          }
         }
       }
     };
